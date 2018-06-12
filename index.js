806c7ff189df0e7fe755eefdba758206837f452c
judell <jon@jonudell.info>
2018-06-11 17:54:30 -0700

    add group fields, expand doc

--- index.js
+++ index.js
@@ -35,6 +35,26 @@ var argsDestDomain = {
     msg: 'domain to which to copy (e.g. site2.net)'
 };
 hlib.createNamedInputForm(argsDestDomain);
+var argsSourceGroup = {
+    element: hlib.getById('sourceGroupContainer'),
+    name: 'source group',
+    id: 'sourceGroup',
+    value: '',
+    onchange: '',
+    type: '',
+    msg: 'group from which to copy annotations'
+};
+hlib.createNamedInputForm(argsSourceGroup);
+var argsDestGroup = {
+    element: hlib.getById('destinationGroupContainer'),
+    name: 'destination group',
+    id: 'destinationGroup',
+    value: '',
+    onchange: '',
+    type: '',
+    msg: 'group to which to copy annotations'
+};
+hlib.createNamedInputForm(argsDestGroup);
 var destinationDomainContainer = hlib.getById('destinationDomainContainer');
 var sourceGroupContainer = hlib.getById('sourceGroupContainer');
 var destinationGroupContainer = hlib.getById('destinationGroupContainer');
